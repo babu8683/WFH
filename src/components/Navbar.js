d5baedfc7d8c6beb8b0afc830bbcce2d95be0b7e
Nitish Kumar <kumarnitish4384@gmail.com>
2023-08-19 12:35:37 +0530

    NAvbar Updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,6 @@ import { IconContext } from "react-icons";
 import Logo from "../assest/nav-logo.jpeg";
 import axios from "axios";
 
-
 function Navbar() {
   const [sidebar, setSidebar] = useState(false);
   const [dropdownVisible, setDropdownVisible] = useState(false);
@@ -17,20 +16,20 @@ function Navbar() {
   const toggleDropdown = () => setDropdownVisible(!dropdownVisible);
   console.log(dropdownVisible);
 
-  const logout =() => {
-    axios.get("http://localhost:3004/logout")
-    .then(function(res){
-      console.log(res.data.statusCode);
-      if (res.data.statusCode == 200) {
-        alert("check");
-        return (window.location.href = "http://localhost:3000/");
-      }
-    })
-    .then(function(err) {
-      console.log(err)
-    })
-    ;
-  }
+  const logout = () => {
+    axios
+      .get("http://localhost:3004/logout")
+      .then(function (res) {
+        console.log(res.data.statusCode);
+        if (res.data.statusCode == 200) {
+          alert("check");
+          return (window.location.href = "http://localhost:3000/");
+        }
+      })
+      .then(function (err) {
+        console.log(err);
+      });
+  };
 
   return (
     <>
@@ -40,9 +39,9 @@ function Navbar() {
             <FaIcons.FaBars onClick={showSidebar} />
           </Link>
 
-        <Link to='/'>
+          <Link to="/">
             <h2 className="logo-title">Logo </h2>
-            </Link>
+          </Link>
           <div className="logo" onClick={toggleDropdown}>
             <p className="welcome-user">Welcome User</p>
             <img src={Logo} alt="Logo" />
@@ -71,14 +70,8 @@ function Navbar() {
         {/* Dropdown */}
         {dropdownVisible && (
           <ul className="dropdown-menu">
-<<<<<<< HEAD
             <li>Edit Profile</li>
             <li onClick={logout}>Logout</li>
-            <li >Ma  le</li>
-=======
-            <li>View Profile</li>
-            <li>Logout</li>
->>>>>>> 9dc32e92bf39378b93caf675379b7fbab5076a9b
           </ul>
         )}
       </IconContext.Provider>
